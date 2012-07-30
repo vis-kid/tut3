1ad21d97bca39a196cdad87c2aae2cc71da8c96a
ed wassermann <visavis.kid@gmail.com>
2012-07-30 22:52:55 +0200

    finish updating users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,22 +16,42 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: "1")
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+
 
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
 
+  
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
 
+  
+
   describe "when user name is too long" do
   	before { @user.name = "a" * 51 }
   	it { should_not be_valid }
   end
+
+  
 
   describe "when email format is invalid" do
   	it "should be invalid" do
@@ -43,6 +63,8 @@ describe User do
 	  end
   end
 
+  
+
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[ user@foo.com A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn ]
@@ -53,6 +75,8 @@ describe User do
     end
   end
 
+  
+
   describe "when email address is already taken" do
     before do
       user_with_duplicate_email = @user.dup
@@ -62,33 +86,47 @@ describe User do
     it { should_not be_valid }
   end
 
+  
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
+
+  
 
   describe "when password and password_confirmation mismatch" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
+  
+
   describe "when password_confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
+
+  
 
   describe "when password is too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should_not be_valid }
   end
 
+  
+
   describe "return value of the authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
 
+  
+
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
+
+  
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
@@ -96,6 +134,8 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+  
 
   describe "remember_token" do
     before { @user.save }
